2f4536a946a55e142eb4aadac2bfa2bd17b0e0c8
구윤지 <younearth@gmail.com>
2024-04-14 16:47:29 +0900

    rootScreen 추가 - bottom navigator추가함

---
+++ 003 Code/hanbat_capstone/lib/screen/root_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'review_screen.dart';
+import 'setting_screen.dart';
+
+class RootScreen extends StatefulWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  State createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: renderChildren.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: renderBottomNavigation(),
+    );
+  }
+
+  List<Widget> renderChildren = <Widget>[
+    Container(          // TODO. 캘린더 화면 연결하기
+      child: Center(
+        child: Text(
+            '캘린더'
+        ),
+      ),
+    ),
+    Container(          // TODO. 일정관리 화면 연결하기
+      child: Center(
+        child: Text(
+            '하루일정'
+        ),
+      ),
+    ),
+    Container(          // TODO. 일정등록 화면 연결하기
+      child: Center(
+        child: Text(
+            '일정추가'
+        ),
+      ),
+    ),
+    ReviewScreen(), // 회고관리 화면 연결
+    SettingScreen() // 설정관리 화면 연결
+  ];
+
+  BottomNavigationBar renderBottomNavigation(){
+    return BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.calendar_month
+              ),
+              label: '캘린더',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                Icons.calendar_view_day
+            ),
+            label: '하루일정',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                Icons.add_circle
+            ),
+            label: '일정추가',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                Icons.fact_check
+            ),
+            label: '회고관리',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                Icons.settings
+            ),
+            label: '설정',
+          )
+        ]
+    );
+  }
+}
