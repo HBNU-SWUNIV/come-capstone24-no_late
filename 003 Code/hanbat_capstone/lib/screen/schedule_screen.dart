0aa2cc618aa5bebec183e359789d0dbc22d0d354
D:\캡스톤 sourcetree <the.song.for@gamil.com>
2024-04-16 14:03:21 +0900

    schedule_screen.dart 커밋

---
+++ 003 Code/hanbat_capstone/lib/screen/schedule_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScheduleScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Text('일정 관리 화면'),
+        )
+    );
+  }
+}
