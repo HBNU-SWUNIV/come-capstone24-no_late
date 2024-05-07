ea0aa3b3678b32c28cb2638c68ae22b1dc421124
구윤지 <younearth@gmail.com>
2024-05-07 11:08:24 +0900

    파이어베이스 프로젝트 설정

--- 003 Code/hanbat_capstone/lib/main.dart
+++ 003 Code/hanbat_capstone/lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hanbat_capstone/screen/root_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 파이어베이스 프로젝트 설정
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MaterialApp(
       theme: ThemeData(
